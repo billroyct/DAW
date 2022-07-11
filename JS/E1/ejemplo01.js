96e330406cc7ffcda7a353b18ab726ad52e4fdf3
unknown <billroycarhuamaca@gmail.com>
2022-07-11 07:18:39 -0500

    Primer commit

---
+++ JS/E1/ejemplo01.js
@@ -0,0 +1,11 @@
+<div class="flex ...">
+    <div class="flex-none w-14 h-14 ...">
+      01
+    </div>
+    <div class="grow h-14 ...">
+      02
+    </div>
+    <div class="flex-none w-14 h-14 ...">
+      03
+    </div>
+  </div>
---
+++ JS/E1/ejemplo04.js
@@ -0,0 +1,11 @@
+let PC = {
+    Procesador : "Intel Core i5",
+    Ram : 32,
+    Marca : "Lenovo",
+    Board : "G480",
+    detalle() {
+        console.log("Procesador: "+this.Procesador+"Ram: "+this.Ram);
+    }
+}
+PC.detalle();
+
